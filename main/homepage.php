2a7537d6e2fb7b2917c4e1033a71099aaa82cf58
Soul181 <s.solonetc@mail.ru>
2021-12-07 23:40:58 +0300

    test_commit_001

--- main/homepage.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = include __DIR__ . '/../database/start.php';
-
-$posts = $db -> getAll('posts');
-
-include __DIR__ . '/../index.view.php';
-?>
--- show.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'functions.php';
-$db = include '/database/start.php';
-$id = $_GET['id'];
-$post = $db -> getOne('posts', $id);
-dd($post['id'] . " - ID страницы вывод");
-
-?>
